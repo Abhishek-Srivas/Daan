faafd7648adc47e018941c621458b22c874a3c06
h01sachan <sachan.himanshu2001@gmail.com>
2021-05-29 02:42:32 +0530

    Backend : ngo login and isAuth middleware added

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -26,14 +26,21 @@ const ngoSignupSchema = yup.object({
     confirmPassword: yup
         .string()
         .required()
-        .oneOf([yup.ref("password"), null], "Passwords must match")
-
+        .oneOf([yup.ref("password"), null], "Passwords must match"),
+    otp: yup.string()
 });
 
-const otpSchema = yup.object({
-    email: yup.string().required().email(),
-    otp : yup.string().required()
-});
+const ngoLoginSchema = yup.object().shape({
+    email: yup.string().ensure().when('contact', {
+        is: '',
+        then: yup.string().required()
+    }),
+    contact: yup.string().ensure().when('email', {
+        is: '',
+        then: yup.string().required()
+    }),
+    password: yup.string().required()
+}, [['contact', 'email', 'password']]);
 
 exports.signup = async (req, res, next) => {
     try {
@@ -45,42 +52,81 @@ exports.signup = async (req, res, next) => {
                 throw new MyError(400, err.errors?.[0]);
             });
         const ngoExist = await Ngo.findOne({ email: data.email });
-        if (ngoExist) throw new MyError(409, "User with this email already exists.");
+        if (ngoExist) throw new MyError(409, "User with this email or mobile already exists.Continue to login");
 
+        let otp = OtpGenerator.generate(4, {
+            alphabets: false,
+            specialChars: false,
+            upperCase: false,
+        })
+        const optdata = await new Otp({
+            email: data.email,
+            otp: otp
+        });
+        console.log(otp);
+        await optdata.save();
+
+        var mailOptions = {
+            from: process.env.Email,
+            to: data.email,
+            subject: "signup successful",
+            html: `<h1>welcome to Daan please verify your email using this otp : ${otp}</h1>`
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                res.send('error') // if error occurs send error as response to client
+            } else {
+                console.log('Email sent');
+                res.send('Sent Successfully')//if mail is sent successfully send Sent successfully as response
+            }
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.otpVerification = async (req, res, next) => {
+    try {
+        const data = await ngoSignupSchema
+            .validate({
+                ...req.body,
+            })
+            .catch((err) => {
+                throw new MyError(400, err.errors?.[0]);
+            });
+        const otpExist = await Otp.findOne({ email: data.email });
+
+        if (!otpExist) throw new MyError(404, "Otp Expired");
+
+        const ngoExist = await Ngo.findOne({
+            $or: [
+                { contact: data.contact },
+                { email: data.email }
+            ]
+        });
+
+        if (ngoExist) throw new MyError(409, "Continue to login ");
+
+        if (otpExist.otp !== data.otp) throw new MyError(409, "Wrong Otp");
 
         const hashPassword = bcrypt.hashSync(data.password, 10);
         delete data.password;
         delete data.confirmPassword;
+
         try {
             const ngo = await Ngo.create({ ...data, password: hashPassword });
-            let otp = OtpGenerator.generate(4, {
-                alphabets: false,
-                specialChars: false,
-                upperCase: false,
-            })
-            const optdata = await new Otp({
-                email: data.email,
-                otp: otp
-            });
-            console.log(otp);
+            const token = JWT.sign({ _id: ngo._id }, process.env.SUPERSECRET, { expiresIn: '6h' });
+            ngo.token = token;
+            await ngo.save();
 
-            await optdata.save();
-            var mailOptions = {
-                from: "sachan.himanshu2001@gmail.com",
-                to: data.email,
-                subject: "signup successful",
-                html: `<h1>welcome to Daan please verify your email using this otp : ${otp}</h1>`
-            };
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                    res.send('error') // if error occurs send error as response to client
-                } else {
-                    console.log('Email sent');
-                    res.send('Sent Successfully')//if mail is sent successfully send Sent successfully as response
-                }
+            await otpExist.delete();
+            res.json({
+                success: true,
+                token: token,
+                data: data
             });
-
         } catch (error) {
             console.log(error.message);
             throw new MyError(503, "Mongo Error.");
@@ -90,45 +136,46 @@ exports.signup = async (req, res, next) => {
     }
 };
 
-exports.otpVerification = async (req,res,next)=>{
+exports.login = async (req, res, next) => {
     try {
-        const data = await otpSchema
+        const data = await ngoLoginSchema
             .validate({
                 ...req.body,
             })
             .catch((err) => {
                 throw new MyError(400, err.errors?.[0]);
             });
-        const otpExist = await Otp.findOne({email : data.email});
+        // console.log(data.password);
+            const ngo = await Ngo.findOne({
+                $or: [
+                    { contact: data.contact },
+                    { email: data.email }
+                ]
+            });
 
-        if(!otpExist) throw new MyError(404, "Otp Expired");
+            if (!ngo) throw new MyError(404, "Ngo Not Found");
+            //chekc if password entered is correct or not
+            if (!bcrypt.compareSync(data.password, ngo.password))
+                throw new MyError(403, "Invalid password");
 
-        const ngoExist = await Ngo.findOne({email : data.email});
 
-        if(ngoExist.isVerified === true) throw new MyError(409, "Continue to login already verified");
-
-        if(otpExist.otp !== data.otp) throw new MyError(409, "Wrong Otp");
-
-        const token=JWT.sign({_id:ngoExist._id},process.env.SUPERSECRET,{expiresIn:'6h'});
-
-        ngoExist.isVerified = true;
-        await ngoExist.save();
-
-        res.json({
-            success: true,
-            token : token,
-            data : ngoExist
-          });
-
-    } catch (error) {
-        next(error);
-    }
-};
-
-exports.resendOtp = async ( req ,res, next) =>{
-    try {
-        
+            const token = JWT.sign({ _id: ngo._id }, process.env.SUPERSECRET, { expiresIn: '6h' });
+            ngo.token = token;
+            await ngo.save();
+            res.json({
+                success: true,
+                token: token,
+                data: ngo
+            });
     } catch (error) {
         next(error)
     }
 };
+
+exports.isAuthCheck = async (req,res,next) =>{
+    try {
+        res.send("isAuth middleware working")
+    } catch (error) {
+        next(error);
+    }
+}
