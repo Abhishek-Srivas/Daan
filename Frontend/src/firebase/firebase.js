9f500f783a813af603d49f4cc391b136038a1fa4
h01sachan <sachan.himanshu2001@gmail.com>
2021-05-30 06:45:05 +0530

    firebase added

---
+++ Frontend/src/firebase/firebase.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCHtXaqwA8cQ4NXlVCppjraup5JvDV3nR8",
+    authDomain: "daan-72497.firebaseapp.com",
+    projectId: "daan-72497",
+    storageBucket: "daan-72497.appspot.com",
+    messagingSenderId: "1008094356987",
+    appId: "1:1008094356987:web:90f53dffd22f98dd7b175e",
+    measurementId: "G-981XXHZP9S"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
